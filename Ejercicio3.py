678703cb219f7401d00f9aa4df0dad825dae07fb
emmabeni27 <larisabeni@mail.com>
2024-08-02 02:00:09 -0300

    modulo

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -3,5 +3,5 @@ import math
 numbers = [10**i for i in range(1, 26)]
 for i in numbers:
     limit = (1+(1/i))**i
-    error = limit/math.e
-    print(f"N: ", i, "Limite: ", limit, "Error absoluto: ", error, "Error relativo: ", error/i)
+    absolute_error = abs(limit/math.e)
+    print(f"N: ", i, "Limite: ", limit, "Error absoluto: ", absolute_error, "Error relativo: ", absolute_error/i)

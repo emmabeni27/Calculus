16e3ba85e48669e813f6e88c57c6abe98d19226b
emmabeni27 <larisabeni@mail.com>
2024-08-02 01:50:21 -0300

    ejercicio 3

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,7 +1,7 @@
 import math
 
-numbers = [x for x in range (0, 100000000000000000000000000)]
+numbers = [10**i for i in range(1, 26)]
 for i in numbers:
-    limit = (1+(1/i))^i
+    limit = (1+(1/i))**i
     error = limit/math.e
-    print(f"N: ", i, "(1+(1/n))^n: ", limit, "error: ", error)
+    print(f"N: ", i, "(1+(1/n))^n: ", limit, "error absoluto: ", error, "error relativo: ", error/i)

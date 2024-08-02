d35a1585c21457297c704d8d3e0440eb50eeef5b
emmabeni27 <larisabeni@mail.com>
2024-08-02 13:04:06 -0300

    EXPONENTES NEGATIVOS

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -6,11 +6,11 @@ def limit_sin(h,x):
     return value
 
 def chart_sin():
-    range_of_h = [10**i for i in range(1, 26)]
+    range_of_h = [10**(-i) for i in range(1, 26)]
     for h in range_of_h:
         print(f"Sin(X): ", limit_sin(h, 1),"  ",limit_sin(h, 0.5),"  ",limit_sin(h, 0.1), "  ",limit_sin(h, 0.05),"  ",limit_sin(h, 0.01),"  ",limit_sin(h, 0))
-
-#chart_sin()
+    print("END")
+chart_sin()
 
 #-----------------------------------------------------------------------------------------------------------------------
 
@@ -19,11 +19,12 @@ def limit_cos(h,x):
     return value
 
 def chart_cos():
-    range_of_h = [10**i for i in range(1, 26)]
+    range_of_h = [10**(-i) for i in range(1, 26)]
     for h in range_of_h:
         print(f"Cos(X): ", limit_cos(h, 1),"  ",limit_cos(h, 0.5),"  ",limit_cos(h, 0.1), "  ",limit_cos(h, 0.05),"  ",limit_cos(h, 0.01),"  ",limit_cos(h, 0))
+    print("END")
 
-#chart_cos()
+chart_cos()
 
 #-----------------------------------------------------------------------------------------------------------------------
 
@@ -32,8 +33,9 @@ def limit_quadratic(h, x):
     return value
 
 def chart_quadratic():
-    range_of_h = [10**i for i in range(1, 100)]
+    range_of_h = [10**(-i) for i in range(1, 100)]
+    print(range_of_h)
     for h in range_of_h:
         print(f"Quadratic(X): ", limit_quadratic(h, 1),"  ",limit_quadratic(h, 0.5),"  ",limit_quadratic(h, 0.1), "  ",limit_quadratic(h, 0.05),"  ",limit_quadratic(h, 0.01),"  ",limit_quadratic(h, 0)) #para ahorrarme el fstring largo podría construir una lista, usando un for, donde almaceno los valores de la función evaluada y acá los pongeo, pero no me ahorro líneas ni bajo el orden
-
-#chart_quadratic()
+    print("END")
+chart_quadratic()

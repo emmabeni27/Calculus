0f9e7f2375e1344c16733d0eca86288c1c5b9849
emmabeni27 <larisabeni@mail.com>
2024-08-02 12:47:30 -0300

    ejercicio 4

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -5,3 +5,5 @@ for i in numbers:
     limit = (1+(1/i))**i
     absolute_error = abs(limit/math.e)
     print(f"N: ", i, "Limite: ", limit, "Error absoluto: ", absolute_error, "Error relativo: ", absolute_error/i)
+
+#se ve afectado el valor para n muy grande debido a la aritmética de punto flotante
---
+++ Ejercicio4.py
@@ -0,0 +1,39 @@
+import math
+
+
+def limit_sin(h,x):
+    value = (math.sin(x+h)-math.sin(x))/h
+    return value
+
+def chart_sin():
+    range_of_h = [10**i for i in range(1, 26)]
+    for h in range_of_h:
+        print(f"Sin(X): ", limit_sin(h, 1),"  ",limit_sin(h, 0.5),"  ",limit_sin(h, 0.1), "  ",limit_sin(h, 0.05),"  ",limit_sin(h, 0.01),"  ",limit_sin(h, 0))
+
+#chart_sin()
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+def limit_cos(h,x):
+    value = (math.cos(x+h)-math.cos(x))/h
+    return value
+
+def chart_cos():
+    range_of_h = [10**i for i in range(1, 26)]
+    for h in range_of_h:
+        print(f"Cos(X): ", limit_cos(h, 1),"  ",limit_cos(h, 0.5),"  ",limit_cos(h, 0.1), "  ",limit_cos(h, 0.05),"  ",limit_cos(h, 0.01),"  ",limit_cos(h, 0))
+
+#chart_cos()
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+def limit_quadratic(h, x):
+    value = ((x + h)**2 - x**2) / h
+    return value
+
+def chart_quadratic():
+    range_of_h = [10**i for i in range(1, 100)]
+    for h in range_of_h:
+        print(f"Quadratic(X): ", limit_quadratic(h, 1),"  ",limit_quadratic(h, 0.5),"  ",limit_quadratic(h, 0.1), "  ",limit_quadratic(h, 0.05),"  ",limit_quadratic(h, 0.01),"  ",limit_quadratic(h, 0)) #para ahorrarme el fstring largo podría construir una lista, usando un for, donde almaceno los valores de la función evaluada y acá los pongeo, pero no me ahorro líneas ni bajo el orden
+
+#chart_quadratic()

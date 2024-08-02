f61442ce4a74fdb76aa4d2fd77ea8b37a9ddad01
emmabeni27 <larisabeni@mail.com>
2024-08-02 01:54:54 -0300

    ejercicio 3, nombres

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -4,4 +4,4 @@ numbers = [10**i for i in range(1, 26)]
 for i in numbers:
     limit = (1+(1/i))**i
     error = limit/math.e
-    print(f"N: ", i, "(1+(1/n))^n: ", limit, "error absoluto: ", error, "error relativo: ", error/i)
+    print(f"N: ", i, "Limite: ", limit, "Error absoluto: ", error, "Error relativo: ", error/i)

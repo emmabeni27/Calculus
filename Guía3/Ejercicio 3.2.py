2bfff9e606ea173228cd1642515f3a48f49ebfd5
emmabeni27 <larisabeni@mail.com>
2024-08-12 09:48:35 -0300

    ejercicio 3.2

---
+++ Guía3/Ejercicio 3.2.py
@@ -0,0 +1,21 @@
+#MÉTODO DE BISECCIÓN
+
+
+def métodoBisección(inicio, fin, función):
+    promedio = (inicio + fin) / 2
+
+    bajo = eval(función.replace('x', str(inicio)).replace('X', str(inicio)))
+    medio = eval(función.replace('x', str(promedio)).replace('X', str(promedio)))
+    alto = eval(función.replace('x', str(fin)).replace('X', str(fin)))              # no olvidar la función eval!!!
+
+    if abs(medio) <= (10 ** (-2)):  #si no aclaro que es el absoluto, podría tomar a -0.29 como correcto, que es mayor a 0.01
+        return promedio
+
+    elif bajo * medio < 0:
+        return métodoBisección(inicio, promedio, función)
+
+    elif medio * alto < 0:
+        return métodoBisección(promedio, fin, función)
+
+
+print(métodoBisección(1, 2, "x**3-x-1"))

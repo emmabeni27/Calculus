e0937f4524902e01adb198a9c6f810c3fad5ed4b
emmabeni27 <larisabeni@mail.com>
2024-08-13 15:40:37 -0300

    grafico

--- Gráficos.py
+++ Gráficos.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
-plt.plot([5,10,6,-10,15,1])
+plt.plot([5,10,6,-10,15,1]) #paso los puntos pero igual los une. Si le paso un sólo string los considera de y, los valores de x los genera automáticament.
 plt.show()
 
-plt.plot([5,10,6,-10,15,1])
+plt.plot([10,25,30,60,70,100], [100,200,-100,300,0,-250]) #el primer vector son las x y el segundo vector son las y
 plt.show()
 

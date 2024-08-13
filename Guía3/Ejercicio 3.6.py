ecbcf58aa84356f4daedc98d6a22a19b0bd9814b
emmabeni27 <larisabeni@mail.com>
2024-08-12 23:33:06 -0300

    ejercicio 3.6

---
+++ Guía3/Ejercicio 3.6.py
@@ -0,0 +1,19 @@
+def métodoBisección(inicio, fin, función):
+    promedio = (inicio + fin) / 2
+
+    bajo = eval(función.replace('x', str(inicio)).replace('X', str(inicio)))
+    medio = eval(función.replace('x', str(promedio)).replace('X', str(promedio)))
+    alto = eval(función.replace('x', str(fin)).replace('X', str(fin)))              # no olvidar la función eval!!!
+
+    if abs(medio) <= (10 ** (-4)):  #si no aclaro que es el absoluto, podría tomar a -0.29 como correcto, que es mayor a 0.01
+        return promedio
+
+    elif bajo * medio < 0:
+        return métodoBisección(inicio, promedio, función)
+
+    elif medio * alto < 0:
+        return métodoBisección(promedio, fin, función)
+
+    else:
+        return promedio
+print(métodoBisección(1, 2, "x**3-3")) #me tengo que el intervalo cumple existencia y unicidad

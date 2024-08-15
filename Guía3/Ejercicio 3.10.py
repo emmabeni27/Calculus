a49938310efe33c0c06e28671556e51a081e8e40
emmabeni27 <larisabeni@mail.com>
2024-08-15 19:32:51 -0300

    grafico

--- Guía3/Ejercicio 3.10.py
+++ Guía3/Ejercicio 3.10.py
@@ -1,12 +1,21 @@
 def metodoNewtonRaphson(funcion, punto, error):
 
-    h=10**(-1000)
+    h=10**(-10)
 
     f = eval(funcion.replace('x', str(punto)).replace('X', str(punto)))
     derivada = (eval(funcion.replace('x', str(punto + h)).replace('X', str(punto + h))) - f) / h
 
+    if derivada == 0: #nunca esta de mas
+        raise ValueError("La derivada es cero. El método de Newton-Raphson no puede continuar.")
+
     nuevo_punto = punto-(f/derivada)
-    if abs(0-nuevo_punto)<error:
+
+    if abs(0-f)<error:
         return nuevo_punto
     else:
         return metodoNewtonRaphson(funcion, nuevo_punto, error)
+
+
+
+print(metodoNewtonRaphson("x**2-3", 2, 10**(-6)))
+
--- Guía3/Ejercicio 3.11.py
+++ Guía3/Ejercicio 3.11.py
@@ -1,3 +1,5 @@
+import math
+
 def metodoNewtonRaphson(funcion, punto, error):
 
     h=10**(-10) # si es MUUUCHO MAS GRANDE, lo toma como cero y no puede dividir por cero!
@@ -5,11 +7,17 @@ def metodoNewtonRaphson(funcion, punto, error):
     f = eval(funcion.replace('x', str(punto)).replace('X', str(punto)))
     derivada = (eval(funcion.replace('x', str(punto + h)).replace('X', str(punto + h))) - f) / h
 
+    if derivada == 0:
+        raise ValueError("La derivada es cero. El método de Newton-Raphson no puede continuar.")
+
     nuevo_punto = punto-(f/derivada)
 
-    if abs(0-f)<error:
+    if abs(f)<error:
         return nuevo_punto
     else:
         return metodoNewtonRaphson(funcion, nuevo_punto, error)
 
-print(metodoNewtonRaphson("x**3-3", 2, 10**(-4) ))
+print(metodoNewtonRaphson("x**3 - 2*x**2 - 5", 2.5, 10**(-4)))
+print(metodoNewtonRaphson("x-math.cos(x)", math.pi/2, 10**(-4)))
+print(metodoNewtonRaphson("x**3 + 3*x**2 - 1", -3.5, 10**(-4)))
+print(metodoNewtonRaphson("x - 0.8 - 0.2 * math.sin(x)", -1, 10**(-4)))
